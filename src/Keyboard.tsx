a2f8cd726edd2b5ecfe67c619d9db17eae8f0978
Stanimir Dimitrov <evlution63@gmail.com>
2024-05-01 17:33:15 +0300

    refactor: create a Key component

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -9,16 +9,22 @@ export const Keyboard = () => {
     <div className='flex flex-col items-center'>
       <div className='flex'>{renderKeysFromLine(KEYS.line1)}</div>
       <div className='flex'>{renderKeysFromLine(KEYS.line2)}</div>
-      <div className='flex'>{renderKeysFromLine(KEYS.line3)}</div>
+      <div className='flex'>
+        <Key value='DELETE' />
+        {renderKeysFromLine(KEYS.line3)}
+        <Key value='Enter' />
+      </div>
     </div>
   );
 };
 
 const renderKeysFromLine = (line: string) =>
-  line
-    .split('')
-    .map((key) => (
-      <span className='p-4 border border-black m-1 rounded-md font-bold'>
-        {key.toUpperCase()}
-      </span>
-    ));
+  line.split('').map((key) => <Key value={key.toUpperCase()} />);
+
+const Key: React.FunctionComponent<{ value: string }> = ({ value }) => {
+  return (
+    <span className='p-4 border border-black m-1 rounded-md font-bold'>
+      {value}
+    </span>
+  );
+};
